a7663e5c874ace7659fc8a8fa3118e803e1b8a0a
JIACHENG135 <liulaoye135@tamu.edu>
2018-11-21 10:09:29 -0600

    leetcode 1 copy

--- leetcode228_summaryRanges.py
+++ leetcode228_summaryRanges.py
@@ -10,4 +10,29 @@ class Solution:
         :type nums: List[int]
         :rtype: List[str]
         """
-        
+        i = 0
+        counter = 0
+        res = []
+        nums.append(float('inf'))
+        while i<len(nums)-1:
+            print(i)
+            if nums[i+1]!= nums[i]+1:
+                if nums[i+1] != nums[i]:
+#                    print(i)
+                    k = i
+                    while nums[k-1]== nums[k]-1 or nums[k-1] == nums[k]:
+                        k = k-1
+                    if k!=i:
+                        res.append(str(nums[k])+'->'+str(nums[i]))
+                    else:
+                        res.append(str(nums[i]))
+                    
+            else:
+                counter += 1
+            i += 1
+        return res
+                
+    
+a = Solution()
+b = a.summaryRanges([8,10])
+print(b)
